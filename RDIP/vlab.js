da40f0da1b62928f27c4a096defc2c6bc3446d44
Jahnavi Jonnalagadda <jahnavi30nov@outlook.com>
2020-07-18 20:11:04 +0530

    Added a dropdown

---
+++ RDIP/vlab.js
@@ -0,0 +1,4 @@
+function dropDown() {
+	document.getElementById("select").innerHTML = "Form a sentence (Declarative or Interrogative or any other type) from the given words";
+	document.getElementById("select1").innerHTML = "(select the buttons in proper order)";
+}
